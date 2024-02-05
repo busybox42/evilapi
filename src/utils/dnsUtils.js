a1665d01be75e7371fd9aff67c4369c843aa9db8
Alan Denniston <alan.denniston@gmail.com>
2024-02-05 00:10:00 -0500

    updated the dns utils

--- src/utils/dnsUtils.js
+++ src/utils/dnsUtils.js
@@ -1,4 +1,6 @@
 const dns = require("dns").promises;
+const dnsSocket = require("dns-socket");
+const socket = dnsSocket();
 
 const isIpAddress = (host) => {
   return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
@@ -6,19 +8,59 @@ const isIpAddress = (host) => {
   );
 };
 
+async function resolveDnsServer(dnsServer) {
+  if (isIpAddress(dnsServer)) {
+    return dnsServer; // Return the IP if dnsServer is already an IP address
+  } else {
+    try {
+      // Attempt to resolve the hostname to an IP
+      const addresses = await dns.lookup(dnsServer);
+      return addresses.address;
+    } catch (err) {
+      // If the DNS server hostname cannot be resolved
+      throw new Error(`Failed to resolve DNS server hostname: ${dnsServer}`);
+    }
+  }
+}
+
 async function dnsLookup(host, type = "A", dnsServer) {
   try {
-    // Set the DNS server if specified
-    if (dnsServer && isIpAddress(dnsServer)) {
-      dns.setServers([dnsServer]);
-    }
+    // Resolve the DNS server address if it's a hostname, or verify it's a valid IP
+    const resolvedDnsServer = await resolveDnsServer(dnsServer);
 
-    const queryType = type || (isIpAddress(host) ? "PTR" : "A");
-    const addresses = await dns.resolve(host, queryType);
-    return { host, type: queryType, addresses, dnsServer };
-  } catch (err) {
-    console.error("DNS Lookup Error:", err);
-    throw err;
+    return new Promise((resolve, reject) => {
+      const queryType = type || (isIpAddress(host) ? "PTR" : "A");
+
+      socket.query(
+        {
+          questions: [
+            {
+              type: queryType,
+              name: host,
+            },
+          ],
+        },
+        53,
+        resolvedDnsServer,
+        (err, res) => {
+          if (err) {
+            console.error("DNS Lookup Error:", err);
+            reject(new Error("DNS Lookup Error"));
+          } else {
+            // Extract the addresses from the response
+            const addresses = res.answers.map((answer) => answer.data);
+            resolve({
+              host,
+              type: queryType,
+              addresses,
+              dnsServer: resolvedDnsServer,
+            });
+          }
+        }
+      );
+    });
+  } catch (error) {
+    throw error; // This will be caught by the try-catch block in the route handler
   }
 }
 
